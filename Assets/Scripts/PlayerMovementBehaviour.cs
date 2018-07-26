46d255d575e01168db53c1a0b86e59f32da70589
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-07-25 21:21:41 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerMovementBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovementBehaviour : MonoBehaviour
+{
+    private NavMeshAgent PlayerAgent;
+	void Start ()
+    {
+        PlayerAgent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update ()
+    {
+		if(Input.GetButtonDown("Fire2"))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray,out hit,1000))
+            {
+                PlayerAgent.SetDestination(hit.point);
+            }
+        }
+	}
+}
